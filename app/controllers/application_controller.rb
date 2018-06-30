a6342ba8d285e9e4748e3c3884f550ac89a29c06
tam <tamara.acevedoavila@gmail.com>
2018-06-29 21:58:15 -0400

    autenticacion para todo menos index de pics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  private
+  def authenticate_user!
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,7 @@
 class HistoriesController < ApplicationController
   before_action :set_history, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except:  [:index]
+
 
   # GET /histories
   # GET /histories.json
